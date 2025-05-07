d4785f29b2721f7a77e8bf6406884fcf8bd97902
Basith27 <abdul@27technologies.com>
2025-05-07 20:36:29 +0530

    multi player integeration issues resolved

--- backend/src/game/game.module.ts
+++ backend/src/game/game.module.ts
@@ -3,35 +3,24 @@ import { MatchmakingService } from './matchmaking.service';
 import { GameEndService } from './game-end/game-end.service';
 import { RatingService } from './rating/rating.service';
 import { GameManagerService } from './game-manager.service';
-<<<<<<< HEAD
 import { DisconnectionService } from './disconnection.service';
 import { TimerModule } from './timer/timer.module';
 
 @Module({
   imports: [TimerModule],
-=======
-
-@Module({
->>>>>>> main
   providers: [
     MatchmakingService,
     GameEndService,
     RatingService,
     GameManagerService,
-<<<<<<< HEAD
     DisconnectionService,
-=======
->>>>>>> main
   ],
   exports: [
     MatchmakingService,
     GameEndService,
     RatingService,
     GameManagerService,
-<<<<<<< HEAD
     DisconnectionService,
-=======
->>>>>>> main
   ],
 })
 export class GameModule {} 
